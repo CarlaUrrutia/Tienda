1b5431913321548c66ab4929e09f94444ecd60cf
CarlaUrrutia <carl.urrutiav@duocuc.cl>
2026-05-08 23:55:28 -0400

    se crea proyecto
    todos mis cambios

---
+++ gerente/src/main/java/com/example/gerente/service/ClienteService.java
@@ -0,0 +1,10 @@
+package com.example.gerente.service;
+import java.util.List;
+public interface ClienteService {
+    List<ClienteDTO.Response> listarCLiente();
+    ClienteDTO.Response buscarPorId (int id_cliente);
+    ClienteDTO.Response crear(ClienteDTO.Request request);
+    ClienteDTO.Response actualizar(int id_cliente, ClienteDTO.Request request);
+    void eliminar(int id_cliente);
+
+}
